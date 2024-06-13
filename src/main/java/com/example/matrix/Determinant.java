bdc9d2cdfb965a89dff0202dc956144a6b3127f3
thomtax <thomastax03@gmail.com>
2024-06-13 16:54:02 +0200

    added back button

--- src/main/java/com/example/matrix/Determinant.java
+++ src/main/java/com/example/matrix/Determinant.java
@@ -1,13 +1,19 @@
 package com.example.matrix;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.List;
 
 public class Determinant {
     @FXML private TextField id00, id01, id02, id10, id11, id12, id20, id21, id22, idDeterminantValue;
-    @FXML private Button idCalculate;
+    @FXML private Button idBack, idCalculate;
 
     int size = 3;
     List<TextField> txfList;
@@ -24,6 +30,13 @@ public class Determinant {
     public void onCalculateClicked() {
         double determinant = calculateDeterminant(matrix);
         idDeterminantValue.setText(String.valueOf(determinant));
+    }
+
+    public void onBackClicked() throws IOException {
+        FXMLLoader fxmlLoader1 = new FXMLLoader(getClass().getResource("Matrix.fxml"));
+        Parent root = fxmlLoader1.load();
+        Stage stage1 = (Stage) idBack.getScene().getWindow();
+        stage1.setScene(new Scene(root));
     }
 
     public void createMatrix() {
