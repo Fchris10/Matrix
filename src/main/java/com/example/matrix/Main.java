738231edac7517da57cdd9788e33794275ac0ec7
Fchris10 <christian.csfr@gmail.com>
2025-04-28 15:43:01 +0200

    -Added a logo -Added label for "Rank", "Determinant" and "Inverse"

--- src/main/java/com/example/matrix/Main.java
+++ src/main/java/com/example/matrix/Main.java
@@ -3,6 +3,7 @@ package com.example.matrix;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,10 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+        Image logo = new Image("file:///C:/Users/chris/IdeaProjects/Matrix/src/main/resources/com/example/matrix/iconsAndImages/matrix_logo.jpg");
+        stage.getIcons().add(logo);
+
         stage.setTitle("Matrix solver");
         stage.setScene(scene);
         stage.setResizable(false);
